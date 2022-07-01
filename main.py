dec2c5ae6ee2bb3b32371e7a9511f0be10964618
Ayman Abd <ayman.abd889@gmail.com>
2022-07-01 22:45:13 +0300

    code refactor

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ with open('Input/Names/invited_names.txt') as names_file:
     for name in names_file:
         stripped_name = name.strip()
         with open(f'Output/ReadyToSend/letter_for_{stripped_name}.txt', "w+") as new_letter:
-            with open('Input/Letters/starting_letter.txt') as letter:
-                for line in letter:
+            with open('Input/Letters/starting_letter.txt') as template_letter:
+                for line in template_letter:
                     new_letter.write(line.replace(PLACEHOLDER, stripped_name))
